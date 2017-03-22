b5fcb11a1dcc14f6e67185b592501c01e8f285b6
ALUMNADO\anasal <asinsanna@gmail.com>
2017-03-22 08:52:05 +0100

    Prueba source tree

---
+++ multiplicar2.php
@@ -0,0 +1,15 @@
+<?php
+//Definimos una variable con un numero inicial
+$numero=1;
+
+//Empezamos el while
+while ($numero <= 10) {
+  $resultado=$numero*2;
+
+  //Lo sacamos por pantalla
+  echo "2 x $numero = $resultado <br>";
+
+  //Especificamos el incremento de numero
+  $numero++;
+}
+ ?>
